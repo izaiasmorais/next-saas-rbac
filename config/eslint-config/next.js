b5aae642bcbdeed3a108ee8cb9fddffdbe7067bc
Izaias Lima <izaiaslima356@gmail.com>
2024-04-15 21:30:00 -0300

    build: set up eslint and prettier config

---
+++ config/eslint-config/next.js
@@ -0,0 +1,9 @@
+/** @type {import('eslint').Linter.Config} **/
+
+module.export = {
+  extends: ['@rocketseat/eslint-config/next'],
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+  },
+}
