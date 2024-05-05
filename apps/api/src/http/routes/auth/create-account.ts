a7234c053b432d2263c75eeed0a462be53c14c97
Izaias Lima <izaiaslima356@gmail.com>
2024-05-05 13:13:25 -0300

    feat: create user route

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
+import { hash } from 'bcryptjs'
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -14,8 +16,32 @@ export async function createAccount(app: FastifyInstance) {
         }),
       },
     },
-    () => {
-      return 'User created'
+    async (request, reply) => {
+      const { name, email, password } = request.body
+
+      const emailAlreadyRegistered = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (emailAlreadyRegistered) {
+        return reply.status(400).send({
+          message: 'The email is already registered',
+        })
+      }
+
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      })
+
+      return reply.status(201).send()
     }
   )
 }
