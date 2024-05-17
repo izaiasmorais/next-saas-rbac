024632ff6f4368611631fa4aee7f7fad87bcbc20
Izaias Lima <izaiaslima356@gmail.com>
2024-05-17 16:56:31 -0300

    feat: dynamic t3 env import

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -1,9 +1,9 @@
-import { createEnv } from '@t3-oss/env-nextjs'
+const { createEnv } = await import('@t3-oss/env-nextjs')
 import { z } from 'zod'
 
 export const env = createEnv({
-	server: {
-		SERVER_PORT: z.coerce.number().default(3333),
+  server: {
+    SERVER_PORT: z.coerce.number().default(3333),
     DATABASE_URL: z.string().url(),
     JWT_SECRET: z.string(),
     GITHUB_OAUTH_CLIENT_ID: z.string(),
@@ -12,8 +12,8 @@ export const env = createEnv({
   },
   client: {},
   shared: {},
-	runtimeEnv: {
-		SERVER_PORT: process.env.PORT,
+  runtimeEnv: {
+    SERVER_PORT: process.env.PORT,
     DATABASE_URL: process.env.DATABASE_URL,
     JWT_SECRET: process.env.JWT_SECRET,
     GITHUB_OAUTH_CLIENT_ID: process.env.GITHUB_OAUTH_CLIENT_ID,
